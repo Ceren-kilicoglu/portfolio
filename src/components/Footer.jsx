b2f4768a81b5f04c262d90ef1d6196168ebf5e69
Ceren-kilicoglu <cern.yaa@gmail.com>
2025-01-02 23:38:16 +0300

    footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -48,25 +48,26 @@ const Footer = () => {
 
     return (
         <div className='w-full max-w-[1519px] h-[454px] bg-[#F9F9F9] dark:bg-[#252128] relative overflow-hidden'>
-            <div className='w-full max-w-[483px] h-[290px] absolute left-1/2 transform -translate-x-1/2 top-[80px] flex flex-col gap-[24px]'>
-                <h1 className='font-bold text-[36px] sm:text-[48px] leading-[52px] sm:leading-[72px] text-[#4731D3] dark:text-[#8F88FF]'>
+            <div className='w-full max-w-[483px] h-[290px] absolute left-1/2 transform -translate-x-1/2 top-[80px] flex flex-col gap-[24px] text-center sm:text-left'>
+                <h1 className='font-bold text-[36px] sm:text-[48px] lg:text-[48px] leading-[52px] sm:leading-[72px] text-[#4731D3] dark:text-[#8F88FF]'>
                     {heading}
                 </h1>
-                <p className='w-full max-w-[448px] font-normal text-[18px] sm:text-[24px] text-[#120B39] dark:text-whit text-center'>
+                <p className='w-full max-w-[448px] font-normal text-[18px] sm:text-[24px] lg:text-[24px] text-[#120B39] dark:text-whit text-center'>
                     {prompt}
                 </p>
-                <p className='w-auto font-medium text-[16px] sm:text-[20px] leading-[24.2px] underline text-[#4731D3] dark:text-[#8F88FF] ml-0 sm:ml-[125px]'>
+                <p className='w-auto font-medium text-[16px] sm:text-[20px] lg:text-[20px] leading-[24.2px] underline text-[#4731D3] dark:text-[#8F88FF] ml-0 sm:ml-[125px]'>
                     {email}
                 </p>
-                <div className='flex gap-[10px] sm:gap-[15px] justify-center sm:justify-center sm:ml-[175px]'>
+                <div className='flex gap-[10px] sm:gap-[15px] justify-center sm:justify-start sm:ml-[175px]'>
                     {socialIcons.map((icon, index) => (
-                        <a key={index} href={icon.link} target='_blank' rel='noopener noreferrer' className='w-[30px] h-[30px] sm:w-[35px] sm:h-[35px] text-[#4731D3] dark:text-[#8F88FF]'>
+                        <a key={index} href={icon.link} target='_blank' rel='noopener noreferrer' className='w-[30px] sm:w-[35px] h-[30px] sm:h-[35px] text-[#4731D3] dark:text-[#8F88FF]'>
                             <FontAwesomeIcon icon={iconMap[icon.icon]} />
                         </a>
                     ))}
                 </div>
             </div>
         </div>
+
 
 
     );
