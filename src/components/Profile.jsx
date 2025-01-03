9ec756ac4fe750b14d6c330e033895d958704b88
Ceren-kilicoglu <cern.yaa@gmail.com>
2025-01-03 16:55:16 +0300

    responsive added

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -32,45 +32,49 @@ const Profile = () => {
 
     return (
 
-        <div className='profile-container bg-bl dark:bg-d-bl h-[552px] overflow-hidden '>
-            <div className='profile-text w-[156px] h-[48px] absolute left-[280px] top-[1300px]'>
-                <h1 className='text-yellw text-[24px] sm:text-[32px] md:text-[40px] lg:text-[48px] font-bold  leading-[32px] sm:leading-[40px] mb-4 sm:mb-6 md:mb-8 lg:mb-10  sm:mt-4 md:mt-6 lg:mt-8 text-left w-full sm:w-[500px] md:w-[700px] lg:w-[960px]'>
+        <div className='profile-container bg-bl dark:bg-d-bl h-auto overflow-hidden px-4 sm:px-8 py-12'>
+            <div className='profile-text w-full sm:w-[500px] md:w-[700px] lg:w-[960px] mx-auto mb-8'>
+                <h1 className='text-yellw text-[20px] sm:text-[28px] md:text-[36px] lg:text-[40px] font-bold leading-[24px] sm:leading-[32px] mb-4 sm:mb-6 md:mb-8 lg:mb-10 text-left'>
                     {profile.title}
                 </h1>
             </div>
-            <div className='profile-info w-[960px] h-[290.68px] flex gap-[35px] absolute top-[1400px] left-[280px]'>
-                <div className='w-[300px] h-[290.68px] flex flex-col gap-[35px]'>
-                    <h2 className='w-[300px] h-[28px] text-[#FFFFFF] text-medium text-[30px] leading-[28px]'>
-                        {profile.subtitle}
-                    </h2>
-                    <div className='flex'>
-                        <div className={`text-yellw font-semibold text-[16px] leading-[24px] flex flex-col gap-[24px] ${language === "tr" ? 'w-[120px] h-[222px]' : 'w-[105px] h-[222px]'}`}>
-                            <p>{profile.labels.birth}:</p>
-                            <p>{profile.labels.residence}:</p>
-                            <p>{profile.labels.education}:</p>
-                            <p>{profile.labels.preferredRole}:</p>
-                        </div>
-                        <div className={`w-[189px] h-[222px] text-whit font-normal text-[16px] flex flex-col ${language === "tr" ? "gap-[24px] leading-[24px]" : "gap-[25px] leading-[24px]"}`}>
-                            <p>{profile.details.birth}</p>
-                            <p>{profile.details.residence}</p>
-                            <p className='leading-[24px]'>{profile.details.education}</p>
-                            <p>{profile.details.preferredRole}</p>
+            <div className='profile-info flex flex-col lg:flex-row gap-8 lg:gap-16 w-full sm:w-[500px] md:w-[700px] lg:w-[960px] mx-auto'>
+                <div className='flex flex-col sm:flex-row lg:flex-row gap-8 lg:gap-16 w-full'>
+                    <div className='flex flex-col gap-4 lg:gap-6 w-full lg:w-1/3'>
+                        <h2 className='text-[#FFFFFF] text-[16px] sm:text-[20px] md:text-[24px] lg:text-[28px] font-medium leading-[28px] mb-4'>
+                            {profile.subtitle}
+                        </h2>
+                        <div className='flex gap-4 lg:gap-6 items-start'>
+                            <div className={`text-yellw font-semibold text-[16px] leading-[24px] flex flex-col gap-[24px] ${language === "tr" ? 'w-[120px] h-[222px]' : 'w-[105px] h-[222px]'}`}>
+                                <p>{profile.labels.birth}</p>
+                                <p>{profile.labels.residence}</p>
+                                <p>{profile.labels.education}</p>
+                                <p>{profile.labels.preferredRole}</p>
+                            </div>
+                            <div className={`w-[189px] h-[222px] text-whit font-normal text-[16px] flex flex-col ${language === "tr" ? "gap-[24px] leading-[24px]" : "gap-[25px] leading-[28px]"}`}>
+                                <p>{profile.details.birth}</p>
+                                <p>{profile.details.residence}</p>
+                                <p className='leading-[24px]'>{profile.details.education}</p>
+                                <p>{profile.details.preferredRole}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div>
-                    <img className='rounded-[10px] w-[300px] h-[290.68px] object-cover' src={image} alt="Profile" />
-                </div>
-                <div className='w-[300px] h-[290.68px] flex flex-col gap-[100px]'>
-                    <h2 className="w-[300px] h-[25px] text-[#FFFFFF] font-medium text-[30px] leading-[30px]">
-                        {aboutMe.title}
-                    </h2>
-                    <p className='w-[300px] h-[241.68px] text-[#FFFFFF] font-normal text-[18px] leading-[27px]'>
-                        {aboutMe.description}
-                    </p>
+                    <div className='flex justify-start lg:w-1/3'>
+                        <img className='rounded-[10px] w-full h-auto object-cover sm:w-[12px] sm:h-[120px] md:w-[300px] md: md:h-[240px] lg:w-[300px] lg:h-[290.68px]' src={image} alt="Profile" />
+                    </div>
+                    <div className='flex flex-col gap-4 lg:gap-6 w-full lg:w-1/3'>
+                        <h2 className="text-[#FFFFFF] font-medium text-[16px] sm:text-[20px] md:text-[24px] lg:text-[28px] leading-[28px] mb-4">
+                            {aboutMe.title}
+                        </h2>
+                        <p className='text-[#FFFFFF] font-normal text-[12px] sm:text-[14px] md:text-[16px] lg:text-[18px] leading-[28px]'>
+                            {aboutMe.description}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
+
+
 
     );
 };
