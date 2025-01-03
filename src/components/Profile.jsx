f2527887fb07ee8e56e87bc6c56dec2d7ebc6ab0
Ceren-kilicoglu <cern.yaa@gmail.com>
2025-01-03 15:56:14 +0300

    responsive changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -31,13 +31,14 @@ const Profile = () => {
     const { profile, aboutMe, image } = state.profileData;
 
     return (
-        <div className='profile-container bg-bl dark:bg-d-bl h-[552px] overflow-hidden'>
-            <div className='profile-text w-[156px] h-[48px] absolute left-[280px] top-[1400px]'>
-                <h1 className='text-yellw font-semibold text-[48px] leading-[48px]'>
+
+        <div className='profile-container bg-bl dark:bg-d-bl h-[552px] overflow-hidden '>
+            <div className='profile-text w-[156px] h-[48px] absolute left-[280px] top-[1300px]'>
+                <h1 className='text-yellw text-[24px] sm:text-[32px] md:text-[40px] lg:text-[48px] font-bold  leading-[32px] sm:leading-[40px] mb-4 sm:mb-6 md:mb-8 lg:mb-10  sm:mt-4 md:mt-6 lg:mt-8 text-left w-full sm:w-[500px] md:w-[700px] lg:w-[960px]'>
                     {profile.title}
                 </h1>
             </div>
-            <div className='profile-info w-[960px] h-[290.68px] flex gap-[35px] absolute top-[1490px] left-[280px]'>
+            <div className='profile-info w-[960px] h-[290.68px] flex gap-[35px] absolute top-[1400px] left-[280px]'>
                 <div className='w-[300px] h-[290.68px] flex flex-col gap-[35px]'>
                     <h2 className='w-[300px] h-[28px] text-[#FFFFFF] text-medium text-[30px] leading-[28px]'>
                         {profile.subtitle}
@@ -70,6 +71,7 @@ const Profile = () => {
                 </div>
             </div>
         </div>
+
     );
 };
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -29,20 +29,20 @@ const Skills = () => {
     }
 
     return (
-        <div className="skills-container bg-[#F9F9F9] dark:bg-[#252128] h-[450px] overflow-hidden">
-            <div className="flex flex-col justify-between  h-[100px] absolute top-[690px] left-[5%]">
-                <h2 className="w-[150px] h-[58px] text-[#4832D3] dark:text-yellow font-bold text-[28px] leading-[38px]">
+        <div className="skills-container bg-[#F9F9F9] dark:bg-[#252128] flex flex-col items-center justify-start overflow-hidden px-4 sm:px-8 py-12">
+            <div className="flex flex-col lg:flex-row items-start w-full sm:w-[500px] md:w-[700px] lg:w-[960px] lg:gap-6">
+                <h2 className="text-[28px] sm:text-[32px] md:text-[40px] lg:text-[48px] font-bold text-[#4832D3] dark:text-yellow leading-[32px] sm:leading-[40px] lg:leading-[58.09px] mb-4 sm:mb-6 md:mb-8 lg:mb-0 w-auto lg:w-[128px]">
                     {state.data.title}
                 </h2>
-                <div className="grid grid-cols-2 justify-between gap-y-[55px] gap-x-[33px] mt-8">
+                <div className="grid grid-cols-2 gap-8 sm:gap-12 lg:ml-[250px] md:gap-14 w-full sm:w-[500px] md:w-[700px] lg:w-[832px] lg:ml-[-20px]">
                     {state.data.logos.map((logo) => (
-                        <div key={logo.id} className="flex items-center gap-2 ">
+                        <div key={logo.id} className="flex items-center gap-[3vw] sm:gap-[3vw] md:gap-[3vw] lg:gap-6">
                             <img
                                 src={logo.logo}
                                 alt={logo.name}
-                                className="w-[20vw] h-[20vw] object-cover"
+                                className="w-[20vw] h-[20vw] sm:w-[15vw] sm:h-[15vw] md:w-[11vw] md:h-[11vw] lg:w-[120px] lg:h-[120px] object-cover"
                             />
-                            <p className="w-[100px] h-[36px] uppercase text-[#777777] dark:text-white font-medium  leading-[38px] text-[4vw]">
+                            <p className="uppercase text-[#777777] dark:text-white text-[4vw] sm:text-[3vw] md:text-[2vw] lg:text-[24px] font-medium leading-[5vw] sm:leading-[36px] md:leading-[36px] lg:leading-[36px]">
                                 {logo.name}
                             </p>
                         </div>
@@ -50,7 +50,6 @@ const Skills = () => {
                 </div>
             </div>
         </div>
-
 
 
     );
